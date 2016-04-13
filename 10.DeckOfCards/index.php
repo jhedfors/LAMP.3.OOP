202bf92fb938753b97e418548ddda4150e4724a3
Jeff Hedfors <git.j@hedfors.net>
2016-04-13 15:38:30 -0700

    updated 10

--- 10.DeckOfCards/index.php
+++ 10.DeckOfCards/index.php
@@ -1,10 +1,7 @@
 <?php
   class Deck{
-    // public $deck[];
-
     function __construct(){
       $deck =  array();
-
       $suits = ['D','H','S','C'];
       $values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
       for ($i=0; $i < 4; $i++) {
@@ -12,16 +9,11 @@
           $deck[] = new Card($suits[$i],$values[$j]);
         }
       }
-
       $this->deck = $deck;
-      shuffle($deck);
-      var_dump($deck);
     }
-    // public function shuffle(){
-    //   // shuffle($deck) ;
-    //
-    //   var_dump($this);
-    // }
+    public function shuffleCards(){
+      shuffle($this->deck);
+    }
     public function reset(){
       //put stuff here
     }
@@ -39,7 +31,8 @@
     }
   }
   $newDeck = new Deck();
-  // var_dump($newDeck);
-  $newDeck->shuffle();
+  var_dump($newDeck);
+  $newDeck->shuffleCards();
+  var_dump($newDeck);
 
  ?>
